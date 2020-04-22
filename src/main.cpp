337eda551915fe4fe8aea9f1dc9f086beebfbcfa
kadu <kadu@kadu.com.br>
2020-04-22 12:24:58 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,230 @@
+ #include <Arduino.h>
+ #include <SimpleButton.h>
+
+using namespace simplebutton;
+
+#define VERMELHO 1
+#define VERDE    2
+#define AMARELO  3
+#define AZUL     4
+
+#define notaVermelho 150
+#define notaVerde    350
+#define notaAmarelo  550
+#define notaAzul     800
+#define notaERRO     1100
+
+#define ledVermelho D3
+#define botaoVermelho D4
+
+#define ledVerde D8
+#define botaoVerde D7
+
+#define ledAmarelo D6
+#define botaoAmarelo D5
+
+#define ledAzul D2
+#define botaoAzul D1
+
+#define BUZZER D0
+
+// Controladores dos botoes
+Button* bVermelho = NULL;
+Button* bVerde = NULL;
+Button* bAmarelo = NULL;
+Button* bAzul = NULL;
+
+int intervalo=500; //specified in milliseconds
+int musica[30];
+int verificaMusica[30];
+
+int fase = 1;
+bool estouEsperando = false;
+int botaoFoiPressionado = -1;
+int botaoPressionado = 0;
+
+// Verifica se o usuario clicou no botao esperado, se não for o esperado, da erro e reinicia o MCU
+void verifica() {  
+  if(musica[botaoPressionado-1] != botaoFoiPressionado) {
+    Serial.println("!!Errou");
+    tone(BUZZER, notaERRO);
+    for (size_t i = 0; i < 2; i++) {
+      digitalWrite(ledVermelho, HIGH);
+      digitalWrite(ledVerde, HIGH);
+      digitalWrite(ledAmarelo, HIGH);
+      digitalWrite(ledAzul, HIGH);
+      delay(250);
+      digitalWrite(ledVermelho, LOW);
+      digitalWrite(ledVerde, LOW);
+      digitalWrite(ledAmarelo, LOW);
+      digitalWrite(ledAzul, LOW);
+      delay(250);
+    }
+    noTone(BUZZER);
+    delay(500);
+    ESP.restart();
+  }
+}
+
+// Cria musicas aleatoriamete
+void cria_musica() {
+  for (size_t i = 0; i < 30; i++) {    
+    musica[i] = random(1,5);    
+  }
+}
+
+void play(int COLOR=VERMELHO, int delayTime = intervalo) {
+  int _PLAYNOTE = notaVermelho;
+  int _LED = ledVermelho;
+  
+  switch (COLOR)
+  {
+    case VERMELHO:
+      _PLAYNOTE = notaVermelho;
+      _LED = ledVermelho;      
+      break;
+    case VERDE:
+      _PLAYNOTE = notaVerde;
+      _LED = ledVerde;
+      break;
+    case AMARELO:
+      _PLAYNOTE = notaAmarelo;
+      _LED = ledAmarelo;
+      break;
+    case AZUL:
+      _PLAYNOTE = notaAzul;
+      _LED = ledAzul;
+      break;
+
+    default:
+      break;
+  }
+
+  Serial.print("COR => ");
+  Serial.println(COLOR);
+  
+  tone(BUZZER, _PLAYNOTE);
+  digitalWrite(_LED, HIGH);
+  delay(delayTime);
+  noTone(BUZZER);
+  digitalWrite(_LED, LOW);
+  delay(delayTime);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Genius");
+
+  pinMode(ledVermelho, OUTPUT); 
+  pinMode(ledVerde, OUTPUT); 
+  pinMode(ledAmarelo, OUTPUT); 
+  pinMode(ledAzul, OUTPUT); 
+  bVermelho = new ButtonPullup(botaoVermelho);
+  bVerde = new ButtonPullup(botaoVerde);
+  bAmarelo = new ButtonPullup(botaoAmarelo);
+  bAzul = new ButtonPullup(botaoAzul);
+
+  bVermelho->setOnPushed([]() {
+        Serial.println("bVermelho");        botaoFoiPressionado = VERMELHO;
+        // Controladores dos botoes
+        botaoPressionado++;
+        verifica();
+        play(VERMELHO);
+  });
+
+  bVerde->setOnPushed([]() {
+        Serial.println("bVerde");
+        botaoFoiPressionado = VERDE;
+        botaoPressionado++;
+        verifica();
+        play(VERDE);
+  });
+
+  bAmarelo->setOnPushed([]() {
+        Serial.println("bAmarelo");
+        botaoFoiPressionado = AMARELO;
+        botaoPressionado++;
+        verifica();
+        play(AMARELO);
+  });
+
+  bAzul->setOnPushed([]() {
+        Serial.println("bAzul");
+        botaoFoiPressionado = AZUL;
+        botaoPressionado++;
+        verifica();
+        play(AZUL);
+  });
+
+  //#Buzzer
+  pinMode(BUZZER, OUTPUT);     
+  randomSeed(digitalRead(D0));
+
+  for (size_t i = 0; i < 30; i++) {
+    verificaMusica[i] = -1;
+  }
+  
+
+  // Inicia jogo
+  cria_musica(); 
+  play(VERMELHO,100);
+  play(VERDE,100);
+  play(AMARELO,100);
+  play(AZUL,100);
+  play(VERMELHO,50);
+  play(VERDE,50);
+  play(AMARELO,50);
+  play(AZUL,50);
+  delay(1500);
+  Serial.println("Starting game...");
+}
+
+void jogo() {
+  if(!estouEsperando) {
+    for (int i = 0; i < fase; i++) {
+      play(musica[i]);
+    }
+    estouEsperando = true;
+  } else {
+      if(botaoFoiPressionado != -1) {
+        verificaMusica[botaoPressionado-1] = botaoFoiPressionado;
+        if(botaoPressionado == fase) {
+            estouEsperando = false;
+            botaoFoiPressionado = -1;
+            fase++;
+            botaoPressionado = 0;
+            delay(50);
+        }
+        /*
+
+        if(botaoPressionado == fase) {
+          bool isCorrect = true;          
+          for (int i = 0; i < fase; i++)
+          {
+            if(musica[i] != verificaMusica[i] || !isCorrect) {
+              isCorrect = false;
+            }
+          }
+
+          if(isCorrect) {
+            Serial.println("Acertou");
+            estouEsperando = false;
+            botaoFoiPressionado = -1;
+            fase++;
+            botaoPressionado = 0;
+            delay(50);
+          }
+        }
+        */
+      }
+  }
+  delay(100);
+}
+
+void loop() {
+  bVermelho->update();
+  bVerde->update();
+  bAmarelo->update();
+  bAzul->update();
+  jogo();
+} 
